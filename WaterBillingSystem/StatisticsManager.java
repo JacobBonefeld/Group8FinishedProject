bef5b9fae4844eac2d2d2449b5e8be698716e215
Kasper Jørgensen <44469647+Shadeschou@users.noreply.github.com>
2019-10-24 13:21:02 +0200

    Statisic (#4)
    * stastics
    
    * stastics
    
    * lets go

--- WaterBillingSystem/StatisticsManager.java
+++ WaterBillingSystem/StatisticsManager.java
@@ -32,8 +32,6 @@ public class StatisticsManager {
                 System.out.println("Input doesn't match menu number try number 1-2 or 0 for going back");
             }
 
-            inputChoice = in.nextInt();
-
             if (inputChoice == 1) {
                 DB.selectSQL("SELECT SUM(reading_amount) FROM reading_card");
                 waterConsumption = DB.getData();
@@ -49,29 +47,6 @@ public class StatisticsManager {
             DB.selectSQL("SELECT SUM(reading_amount) FROM reading_card INNER JOIN water_meters " +
                     "ON water_meters.serial = reading_card.serial WHERE water_meters.cus_id = " + customerID + "");
             waterConsumption = DB.getData();
-
-            }
-            System.out.println(waterConsumption + " m3");
-            System.out.print("1. Show total consumption\n" +
-                    "2. Show consumption for a costumer");
-
-            inputChoice = in.nextInt();
-
-            if (inputChoice == 1) {
-                DB.selectSQL("SELECT SUM(reading_amount) FROM reading_card");
-                waterConsumption = DB.getData();
-                System.out.print("Total water consumption: ");
-            } else {
-                System.out.println("Input customer ID: ");
-                customerID = in.nextInt();
-
-                DB.selectSQL("SELECT customer_name FROM customer WHERE customer_id = " + customerID + " ");
-                customerName = DB.getData();
-                System.out.print("Water usage from " + customerName + " = ");
-
-                DB.selectSQL("SELECT SUM(reading_amount) FROM reading_card INNER JOIN water_meters " +
-                        "ON water_meters.serial = reading_card.serial WHERE water_meters.cus_id = " + customerID + "");
-                waterConsumption = DB.getData();
 
             }
         }
